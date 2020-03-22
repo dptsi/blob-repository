6c2fd6572076266e9a3df7fdc01eaaa6e40f3762
Umar Hasan <sansanbgl@gmail.com>
2020-03-22 23:39:05 +0700

    Added example

---
+++ test-upload.php
@@ -0,0 +1,35 @@
+<?php 
+error_reporting(1);
+require_once('vendor/autoload.php'); 
+
+use MyITS\BlobRepository\BlobRepository;
+$blobUpload = new BlobRepository('', '');
+
+if(isset($_POST["submit"])) {
+    $d = $blobUpload->upload($_FILES['fileToUpload']);
+    var_dump($d);
+}
+
+$search = true;
+if($search){
+    $d = $blobUpload->getFile('34d04c43ea312eee5b723cdd725fec28-1');
+    $data = $d->response->data;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+<?php if($search): ?>
+    <img src="data:image/jpg;base64,<?=$data?>" />';
+<?php endif; ?>
+
+</body>
+</html>
